8a5d20d3e4fdc8560e91a2900784d5d7e09c157f
Chang Chih Chieh <haninfinity530@gmail.com>
2019-09-16 15:17:50 +0800

    新增手冊說明資訊，調整目錄位置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,10 +6,10 @@ module.exports = {
     themeConfig: {
       sidebarDepth: 0,
       sidebar: [
-        ['/基本介紹/手冊說明', '手冊說明'],
+        ['/', '手冊說明'],
         {
           title: '情資網基本介紹',
-          path: '/',
+          path: '/基本介紹/',
           collapsable: false,
           children: [
             '/基本介紹/進入方法',
